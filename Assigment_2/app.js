b6b45871c0d48d00032d64e5c2080d40ee2f6361
Hamza-Khan-official <kh.hamza56789@gmail.com>
2025-09-07 01:28:09 +0500

    completed

--- Assigment_2/app.js
+++ Assigment_2/app.js
@@ -1,4 +1,4 @@
-// CHAPTERS 9 TO 11
+// CHAPTERS 9 TO 11 (USER INPUT AND CONDITIONAL STATEMENTS)
 
 // Q1.
 let city = prompt("enter your city").toLowerCase();
@@ -42,6 +42,52 @@ if (fuel < 0.25){
     document.write("Please refill the fuel in your car");
 };
 
+
+// Q5.
+// a.
+var a = 4;
+if (++a === 5) {
+  alert("given condition for variable a is true");   //true  
+}
+
+// b.
+var b = 82;
+if (b++ === 83) {
+  alert("given condition for variable b is true");   //false
+}
+
+// c.
+var c = 12;
+if (c++ === 13) {
+  alert("condition 1 is true");   //false
+}
+if (c === 13) {
+  alert("condition 2 is true");  //true
+}
+if (++c < 14) {
+  alert("condition 3 is true");   //false
+}
+if (c === 14) {
+  alert("condition 4 is true"); //true
+}
+
+// d.
+var materialCost = 20000;
+var laborCost = 2000;
+var totalCost = materialCost + laborCost;
+
+if (totalCost === laborCost + materialCost) {
+
+  alert("The cost equals");         //true
+}
+
+// e.
+if (true) {
+  alert("True");   //true
+}
+if (false) {
+  alert("False");   //false
+}
 
 
 // Q6.
@@ -112,10 +158,6 @@ if(secret_number === enter_secret_number){
 document.write(number_message);
 
 
-
-
-
-
 // Q8.
 let divisible_number = prompt("enter a number");
 
@@ -184,3 +226,104 @@ if (operation === "+"){
 document.write(result);
 
 
+// CHAPTER 12-13  (IF ELSE AND ELSE IF STATMENTS)
+
+
+
+// Q2.
+let first_number_check = +prompt("Enter a first Number");
+let Second_number_check = +prompt("Enter a Second Number");
+let message_number_check;
+
+if (first_number_check > Second_number_check){
+    message_number_check = "first  Number is greater than second num";
+
+}else if  (first_number_check < Second_number_check){
+    message_number_check = "Second Number is greater than first num";
+}else{
+    message_number_check = "both are same number";
+}
+
+document.write(message_number_check);
+
+// Q3.
+let number_check_ = +prompt("Enter a Number");
+let number_check_message;
+
+if(number_check_ > 0 ){
+    number_check_message = "Your number is positive";
+
+}else if(number_check_ < 0 ){
+    number_check_message = "your nmber is neagative";
+
+}else if(number_check_ === 0){
+    number_check_message = "Your number is Zero";
+
+}else{
+    number_check_message = "Please enter a vaild number";
+};
+
+document.write(number_check_message);
+
+
+// Q4.
+let take_character = prompt("enter a character").toLowerCase();
+let isvowel;
+
+if(take_character === "a" || take_character === "e" || take_character === "i"|| 
+    take_character === "o"|| take_character === "u"){
+
+        isvowel  = true;
+}else{
+    isvowel = false;
+}
+document.write(isvowel);
+
+// Q5.
+let correct_passward = "hamza_123";
+let user_passward = prompt("Enter your passward");
+let passward_message;
+
+console.log(user_passward);
+
+
+if( user_passward === null || user_passward === ""){
+    passward_message = "Please enter your password";
+
+}else if(user_passward === correct_passward){
+    passward_message = "Correct! The password you entered matches the original password";
+
+}else{
+    passward_message = "Incorrect password";
+};
+document.write(passward_message);
+
+// Q6.
+let hour = 18;
+let greeting;
+
+if(hour < 18){
+    greeting = "Good Day";
+}else {
+    greeting = "Good Evening";
+};
+document.write(greeting);
+
+
+// Q7.
+let take_time = +prompt("Enter You Time");
+let time_message;
+
+if(take_time >= 0 && take_time < 1200 ){
+    time_message = "Good Morning!";
+
+}else if(take_time >= 1200 && take_time < 1700){
+    time_message = "Good afternoon!"
+
+}else if(take_time >= 1700 && take_time < 2100){
+    time_message = "Good evening!"
+
+}else if(take_time >= 2100 && take_time < 2359){
+    time_message = "Good night!"
+}
+document.write(time_message);
