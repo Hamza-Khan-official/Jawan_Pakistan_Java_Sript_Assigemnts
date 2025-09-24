5e71333f7b7bc4c32643a1a8f223424747cb0b25
Hamza-Khan-official <kh.hamza56789@gmail.com>
2025-09-24 08:39:17 -0700

    started

---
+++ Assigment_7/app.js
@@ -0,0 +1,52 @@
+// FUNCTION
+// Q1.
+// function date(){
+//     let date = new Date();
+//     console.log(date);
+// };
+// date();
+
+// Q2.
+// function fullName(){
+//     let first_name = prompt("Enter your first name ");
+//     let last_name = prompt(" Enter your last_name name");
+//     let full_name = first_name + last_name;
+//     console.log("Hy ",full_name); 
+// };
+// fullName();
+
+// Q3.
+// function sum(num_1, num_2){
+//     nums = num_1 + num_2;
+//     return nums;
+// };
+// sum(20, 30);
+// console.log(nums);
+
+// Q4.
+// function calculator(Num_1, Num_2, Num_3){
+//     Num_1 = +prompt("enter a first Num");
+//     Num_2 = +prompt("enter a second Num");
+//     Num_3 = +prompt("enter a third Num");
+//     let nums_operation = prompt("Choose your operation, +,-,x,/");
+
+//     if(nums_operation === "+"){
+//         console.log(Num_1 + Num_2 + Num_3);
+
+//     }else if(nums_operation === "-"){
+//         console.log(Num_1 - Num_2 - Num_3);
+
+//     }else if(nums_operation === "x"){
+//         console.log(Num_1 * Num_2 * Num_3);
+
+//     }else if(nums_operation === "/"){
+//         console.log(Num_1 / Num_2 / Num_3);
+//     }else{
+//         console.log("Please enter a valid number and valid operation");
+//     }
+// };
+
+// calculator();
+
+
+// Q5.
