fd3865f48d04b996ee826bf0ac7fd2ef08bf540c
Hamza-Khan-official <*hamza@79034#>
2025-10-04 13:18:47 +0500

    updated

---
+++ FMP(final module project)/product.js
@@ -0,0 +1,5 @@
+// function tech_spechs_detail_open() {
+//     console.log("Div clicked ✅");
+//     var tech_spechs = document.getElementById('tech_spechs_detail_');
+//     tech_spechs.style.display = "block";  // ab wo visible ho jayega
+// }
